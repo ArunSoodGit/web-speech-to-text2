a8ada17afc8a3c652056d2a4306b3960173ee6c3
Arun <arun.sood1998@gmail.com>
2021-10-23 22:31:00 +0200

    heroku init2

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 //Install express server
+const cors = require('cors')
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.use(cors());
+app.options('*', cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/web-speech-to-text'));
 
 app.get('/*', function(req,res) {
-
   res.sendFile(path.join(__dirname+'/dist/web-speech-to-text/index.html'));
 });
 
