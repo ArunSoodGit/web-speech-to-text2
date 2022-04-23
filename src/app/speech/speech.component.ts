5515ecd2fa9d2484b7571d80dc436d08ae4333a6
Arun <arun.sood1998@gmail.com>
2022-04-23 16:45:03 +0200

    Dodanie możliwości pobierania notatki w foramcie .docx

--- src/app/speech/speech.component.ts
+++ src/app/speech/speech.component.ts
@@ -1,14 +1,16 @@
-import {Component} from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {AzureSpeechService} from '../services/azure-speech.service';
 import {ContentChange, SelectionChange} from 'ngx-quill';
-
+import jsPDF from 'jspdf';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-speech',
   templateUrl: './speech.component.html',
   styleUrls: ['./speech.component.scss']
 })
 export class SpeechComponent {
+
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
   languages = [
     {code: 'pl-PL', language: 'Polish (Poland)'},
     {code: 'en-GB', language: 'English (United Kingdom)'},
@@ -17,17 +19,13 @@ export class SpeechComponent {
   ];
 
   selectedLanguage = 'pl-PL';
-  public Editor = ClassicEditor;
   record = true;
-  public config = {
-    language: 'pl'
-  };
   test: any;
   micClass = 'mic';
-  // }
   speechHasStarted = false;
   hasContent: boolean;
   transcript: any;
+  quillConfig: any;
 
   constructor(public azureService: AzureSpeechService) {
   }
@@ -45,20 +43,34 @@ export class SpeechComponent {
     }
   }
 
+  onSelectionChanged($event: SelectionChange): void {
+  }
+
+  onContentChanged($event: ContentChange): void {
+
+  }
 
   // textCorrection(): void {
   //   this.textCorrectionService.correctText(this.service.text).subscribe(res => {
   //     this.service.finaleText = res.text;
   //     console.log('data response', res.text);
   //   });
-  // }
-  quillConfig: any;
-
-  onSelectionChanged($event: SelectionChange) {
+  saveNoteAsPdfFile(): void {
+    const pdfTable = this.pdfTable.nativeElement;
+    const pdf = new jsPDF();
     console.log(this.azureService.text3);
+    // const file = new File([this.azureService.text3], 'myNote.txt', {type: 'text/plain;charset=utf-8'});
+    // saveAs(file, 'myNote.docx');
+    pdf.html(pdfTable.innerHTML);
+
+    pdf.save('myNote.pdf');
+
+
   }
 
-  onContentChanged($event: ContentChange) {
-
+  saveNoteAsDocxFile(): void {
+    console.log(this.azureService.text3);
+    const file = new File([this.azureService.text3], 'myNote.docx', {type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
+    saveAs(file, 'myNote.docx');
   }
 }
